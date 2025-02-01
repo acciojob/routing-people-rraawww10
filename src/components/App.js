bf86413a4b65d22da267695d967e400c7fcebcd4
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2025-02-01 11:28:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,17 @@
-import { BrowserRouter as Router, Route, Routes, Link, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 
-const UserList = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data));
-  }, []);
-
-  return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">User List</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id} className="mb-2">
-            <Link to={`/user/${user.id}`} className="text-blue-500 hover:underline">
-              {user.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const UserDetails = () => {
-  const { id } = useParams();
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => res.json())
-      .then((data) => setUser(data));
-  }, [id]);
-
-  if (!user) return <p>Loading...</p>;
-
-  return (
-    <div className="p-4">
-      <h2 className="text-xl font-bold">{user.name}</h2>
-      <p>Email: {user.email}</p>
-      <p>Phone: {user.phone}</p>
-      <p>Website: {user.website}</p>
-      <Link to="/" className="text-blue-500 hover:underline mt-4 inline-block">
-        Back to User List
-      </Link>
-    </div>
-  );
-};
-
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<UserList />} />
-        <Route path="/user/:id" element={<UserDetails />} />
+        <Route path="/users/:id" element={<UserDetails />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
